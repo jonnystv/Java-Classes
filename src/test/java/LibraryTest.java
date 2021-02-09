8d0776a058b35b06a1ba75e8bf6df609dcc3f155
jonnystv <jonathan@darefoundation.com>
2021-02-09 23:51:11 +0000

    checkLibraryBookStock test failed

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+    @Before
+    public void before() {
+        library = new Library(2);
+        book1 = new Book("1984", "George Orwell", "Dystopian");
+        book2 = new Book("Neuromancer", "William Gibson", "Cyberpunk");
+        book3 = new Book("The Stainless Steel Rat", "Harry Harrison", "Sci-Fi Comedy");
+    }
+
+    @Test
+    public void checkLibraryBookStock() {
+        assertEquals(0, library.getBookCount());
+    }
 }
