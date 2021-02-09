fa9cb157776f6b94d3c8fa5d5735988890d7f1a3
jonnystv <jonathan@darefoundation.com>
2021-02-09 23:10:46 +0000

    hasTitle test failed

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    Book book1;
+
+    @Before
+    public void before(){
+        book1 = new Book("1984","George Orwell", "Dystopian");
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("1984", book1.getTitle());
+    }
 }
