5d2eb85ec706f110b640346484ee838a859039e7
jonnystv <jonathan@darefoundation.com>
2021-02-09 23:34:52 +0000

    Library class created

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private int capacity;
+    private ArrayList<Book> stockList;
+
+
+    public Library(int capacity) {
+        this.capacity = capacity;
+        this.stockList = new ArrayList<Book>();
+    }
 }
